eb7ae3ecac2e76fc89d9ee4e16101c6961efa01a
MrMiyagi7 <157651526+MrMiyagi7@users.noreply.github.com>
2024-10-20 18:14:34 +0200

    Added function to sort reviews by date and display most recent reviewer

--- couch-surfing/src/index.ts
+++ couch-surfing/src/index.ts
@@ -27,8 +27,24 @@ const reviews = [
   },
 ];
 
-function showTotal(reviewsTotal: number) {
-  reviewTotalDisplay.innerHTML = reviewsTotal.toString();
+function sortByMostRecent(
+  reviews: { name: string; stars: number; loyaltyUser: boolean; date: string }[]
+) {
+  return reviews.sort((a, b) => {
+    // Convert the date strings to Date objects for comparison
+    const dateA = new Date(a.date.split("-").reverse().join("-"));
+    const dateB = new Date(b.date.split("-").reverse().join("-"));
+    return dateB.getTime() - dateA.getTime(); // Sort in descending order (most recent first)
+  });
+}
+const sortedReviews = sortByMostRecent(reviews);
+
+function showTotal(reviewsTotal: number, reviewName: String) {
+  reviewTotalDisplay.innerHTML =
+    "Total number of reviews " +
+    reviewsTotal.toString() +
+    "| Last reviewed by  " +
+    reviewName;
 }
 
-showTotal(reviews.length);
+showTotal(reviews.length, sortedReviews[0].name);
